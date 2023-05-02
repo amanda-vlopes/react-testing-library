0ff8e29ede4f9ceaeb92e8af1eac3a3cb7d65c3b
amanda-vlopes <amandavitor@hotmail.com>
2023-05-02 16:38:34 -0300

    Procurando erro

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { render, screen } from '@testing-library/react';
 import { About } from '../pages';
 
 describe('Componente About contém as informações sobre a Pokédex', () => {
