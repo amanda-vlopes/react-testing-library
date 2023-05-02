239f3f3cebbfd589bdd138ffcfe2d8121353aeb6
amanda-vlopes <amandavitor@hotmail.com>
2023-05-02 20:10:40 -0300

    Requisito 4 finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from './renderWithRouter';
+
+describe('Componente Not Found', () => {
+  test('contém um heading com o texto: Page requested no found', () => {
+    renderWithRouter(<NotFound />);
+    expect(screen.getByRole('heading', { name: /Page requested not found/i, level: 2 })).toBeInTheDocument();
+  });
+  test('contém uma imagem com a src especificada', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByRole('img');
+    const srcImage = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image.src).toContain(srcImage);
+  });
+});
